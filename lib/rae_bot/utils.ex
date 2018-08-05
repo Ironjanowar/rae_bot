213024b4f51eaf8ed3be1498f43b4d0ce8d2f293
Ironjanowar <ironjanowar@gmail.com>
2018-08-05 18:59:51 +0200

    Better way to take an id

--- lib/rae_bot/utils.ex
+++ lib/rae_bot/utils.ex
@@ -1,14 +1,15 @@
 defmodule RaeBot.Utils do
   def generate_inline_response(responses, parse_mode \\ "") do
     responses
-    |> Enum.map(fn [title: title, result: result] ->
+    |> Enum.with_index(1)
+    |> Enum.map(fn {[title: title, result: result], id} ->
       %ExGram.Model.InlineQueryResultArticle{
         input_message_content: %ExGram.Model.InputTextMessageContent{
           message_text: result,
           parse_mode: parse_mode
         },
         title: title,
-        id: :rand.uniform(100),
+        id: id,
         type: "article"
       }
     end)
